5978e4d7eef63e1fa3fd86b2250edeeba95a6dae
Raphael Roy <roy.raphael.33@gmail.com>
2022-09-23 10:55:56 +0200

    feat: add JWT management, for login, and also interceptor and guard for other routes

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    // dumb login : update the email and password variables here
+    const email: string = '';
+    const password: string = '';
+    this.authService.login(email, password);
   }
 
 }
