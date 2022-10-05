fa1207d301e267d528d443f7b820f45dfe6b3397
Raphael Roy <roy.raphael.33@gmail.com>
2022-10-05 15:56:46 +0200

    feat: add TimeAgoPipe and ShortenPipe

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from './material.module';
+import { TimeAgoPipe } from './pipes/time-ago.pipe';
+import { ShortenPipe } from './pipes/shorten.pipe';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TimeAgoPipe,
+    ShortenPipe
+  ],
   imports: [
     CommonModule
   ],
   exports: [
-    MaterialModule
+    MaterialModule,
+    TimeAgoPipe,
+    ShortenPipe
   ]
 })
 export class SharedModule { }
