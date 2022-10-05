fb6cc9a3a7d532cb4a91064a7845d50b591a3f84
Raphael Roy <roy.raphael.33@gmail.com>
2022-10-05 16:00:23 +0200

    feat: manage refresh tokens

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,17 +1,94 @@
-import { HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, catchError, filter, Observable, switchMap, take, throwError  } from 'rxjs';
 import { AuthService } from '../services/auth.service';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+  private isRefreshing = false;
+  private refreshTokenSubject: BehaviorSubject<any> = new BehaviorSubject<any>(null);
+  private accessToken$: BehaviorSubject<any> = new BehaviorSubject<any>(null);
 
   constructor(private authService: AuthService) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const headers = new HttpHeaders()
-      .append('Authorization', `Bearer ${this.authService.getToken()}`);
-    const modifiedReq = req.clone({ headers });
-    return next.handle(modifiedReq);
+    let modifiedReq = req;
+    // console.log(modifiedReq.url);
+
+    if (modifiedReq.url === `${environment.apiUrl}/auth/signup` || modifiedReq.url === `${environment.apiUrl}/auth/login` || modifiedReq.url === `${environment.apiUrl}/auth/logout` || modifiedReq.url === `${environment.apiUrl}/auth/refresh`) {
+      // Auth routes
+      const token = this.authService.getRefreshToken();
+      if (token === null) {
+        // No need of a refresh token for signup, login and logout routes : only refresh route
+        // if (modifiedReq.url.includes('auth/refresh')) {
+        if (modifiedReq.url === `${environment.apiUrl}/auth/refresh`) {
+          this.authService.redirectToLogin();
+          // TODO return error or not ?
+          return throwError(() => new Error ("No refresh token when requesting URL " + modifiedReq.url));
+        }
+      } else {
+        modifiedReq = this.addTokenHeader(req, token);
+      }
+    } else {
+      // Not Auth routes
+      const token = this.authService.getAccessToken();
+      if (token === null) {
+        return this.refreshAndHandle(modifiedReq, next);
+      } else {
+        modifiedReq = this.addTokenHeader(req, token);
+      }
+    }
+
+    return next.handle(modifiedReq).pipe(
+      catchError(error => {
+        if (error instanceof HttpErrorResponse && error.status === 401) {
+          return this.refreshAndHandle(modifiedReq, next);
+        }
+        return throwError(() => error);
+      })
+    );
+  }
+
+  private refreshAndHandle(request: HttpRequest<any>, next: HttpHandler) {
+    if (!this.isRefreshing) {
+      this.isRefreshing = true;
+      this.accessToken$.next(null);
+
+      const token = this.authService.getRefreshToken();
+
+      if (token) {
+        return this.authService.refreshTokenObservable().pipe(
+          switchMap((token: any) => {
+            this.isRefreshing = false;
+
+            this.authService.setAccessToken(token.accessToken);
+            this.authService.setRefreshToken(token.refreshToken);
+            this.accessToken$.next(token.accessToken);
+            
+            return next.handle(this.addTokenHeader(request, token.accessToken));
+          }),
+          catchError((err) => {
+            this.isRefreshing = false;
+            
+            this.authService.logOut();
+            return throwError(() => err);
+          })
+        );
+      } else {
+        this.authService.redirectToLogin();
+        return throwError(() => new Error ("No refresh token when requesting URL " + request.url));
+      }
+    }
+
+    return this.accessToken$.pipe(
+      filter(accessToken => accessToken !== null),
+      take(1),
+      switchMap((accessToken) => next.handle(this.addTokenHeader(request, accessToken)))
+    );
+  }
+
+  private addTokenHeader(request: HttpRequest<any>, token: string) {
+    return request.clone({ headers: request.headers.set('Authorization', 'Bearer ' + token) });
   }
 }
--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,39 +1,166 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { catchError, Observable, of, take, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import jwt_decode, { JwtPayload } from 'jwt-decode';
+
+interface AccessTokenJwtPayload extends JwtPayload {
+  userId: string;
+}
+interface RefreshTokenJwtPayload extends JwtPayload {
+  userId: string;
+  refreshTokenId: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  constructor(private http: HttpClient) {}
+  accessTokenExpirationTimestamp: number = 0;
+  refreshTokenExpirationTimestamp: number = 0;
+
+  constructor(private http: HttpClient,
+              private router: Router) {
+    const accessToken = this.getAccessToken();
+    if (accessToken) this.updateAccessTokenExpiration(accessToken);
+    const refreshToken = this.getRefreshToken();
+    if (refreshToken) this.updateRefreshTokenExpiration(refreshToken);
+  }
   
-  login(email: string, password: string) {
+  // Methods relative to API endpoints
+
+  signup(email: string, password: string): void {
     const body: {email: string, password: string} = {email, password};
-    this.http.post<{userId: string, token: string}>(`${environment.apiUrl}/auth/login`, body).pipe(
+    this.http.post<{message: string}>(`${environment.apiUrl}/auth/signup`, body).pipe(
       take(1),
-      tap(loginResponse => {
-        localStorage.setItem("jwt", loginResponse.token);
-        localStorage.setItem("userId", loginResponse.userId);
+      tap(() => {
+        console.log('User created !');
       }),
       catchError(error => {
-        this.resetToken();
-        console.log('Caught in CatchError. Throwing error');
+        this.resetTokens();
+        console.log('Caught in signup CatchError. Throwing error');
         throw new Error(error);
       })
     ).subscribe();
   }
   
-  getToken(): string | null {
-    return localStorage.getItem("jwt");
+  login(email: string, password: string): void {
+    const body: {email: string, password: string} = {email, password};
+    this.http.post<{userId: string, accessToken: string, refreshToken: string}>(`${environment.apiUrl}/auth/login`, body).pipe(
+      take(1),
+      tap(loginResponse => {
+        this.setUserId(loginResponse.userId);
+        this.setAccessToken(loginResponse.accessToken);
+        this.setRefreshToken(loginResponse.refreshToken);
+      }),
+      catchError(error => {
+        this.resetTokens();
+        console.log('Caught in login CatchError. Throwing error');
+        console.log(error);
+        throw new Error(error);
+      })
+    ).subscribe();
   }
+
+  logOut(): void {
+    this.http.post<{}>(`${environment.apiUrl}/auth/logout`, {}).pipe(
+      take(1),
+      tap(() => {
+        this.resetTokens();
+      }),
+      catchError(error => {
+        console.log('Caught in logOut CatchError. Throwing error');
+        throw new Error(error);
+      })
+    ).subscribe();
+  }
+  
+  refreshTokenObservable(): Observable<{userId: string, accessToken: string, refreshToken: string}> {
+    return this.http.post<{userId: string, accessToken: string, refreshToken: string}>(`${environment.apiUrl}/auth/refresh`, {});
+  }
+
+  // Getter / Setters
   
   getUserId(): string | null {
     return localStorage.getItem("userId");
   }
   
-  resetToken() {
-    localStorage.removeItem("jwt");
+  setUserId(id: string): void {
+    window.localStorage.setItem("userId", id);
+  }
+  
+  getAccessToken(): string | null {
+    var hasExpired = false;
+    if (this.accessTokenExpirationTimestamp !== 0) {
+      hasExpired = this.accessTokenExpirationTimestamp > Date.now();
+    }
+    return hasExpired ? null : localStorage.getItem("accessToken");
+  }
+  
+  setAccessToken(token: string): void {
+    window.localStorage.setItem("accessToken", token);
+    this.updateAccessTokenExpiration(token);
+  }
+
+  getRefreshToken(): string | null {
+    var hasExpired = false;
+    if (this.refreshTokenExpirationTimestamp !== 0) {
+      hasExpired = this.refreshTokenExpirationTimestamp > Date.now();
+    }
+    return hasExpired ? null : localStorage.getItem("refreshToken");
+  }
+  
+  setRefreshToken(token: string): void {
+    window.localStorage.setItem("refreshToken", token);
+    this.updateRefreshTokenExpiration(token);
+  }
+
+  // Other methods
+
+  updateAccessTokenExpiration(token: string): void {
+    // Retrieve expiration time and store it
+    const decodedPayload = jwt_decode<AccessTokenJwtPayload>(token);
+    if (decodedPayload === null) {
+      console.error("Error when decoding accessToken : " + token);
+    } else {
+      const decodedPayloadExpiration = decodedPayload.exp;
+      if (decodedPayloadExpiration === undefined) {
+        console.error("Error when reading accessToken payload (no exp field) : " + decodedPayload);
+      } else {
+        this.accessTokenExpirationTimestamp = decodedPayloadExpiration * 1000;
+        return;
+      }
+    }
+    this.accessTokenExpirationTimestamp = 0;
+  }
+
+  updateRefreshTokenExpiration(token: string): void {
+    // Retrieve expiration time and store it
+    const decodedPayload = jwt_decode<RefreshTokenJwtPayload>(token);
+    if (decodedPayload === null) {
+      console.error("Error when decoding refreshToken : " + token);
+    } else {
+      const decodedPayloadExpiration = decodedPayload.exp;
+      if (decodedPayloadExpiration === undefined) {
+        console.error("Error when reading refreshToken payload (no exp field) : " + decodedPayload);
+      } else {
+        this.refreshTokenExpirationTimestamp = decodedPayloadExpiration * 1000;
+        return;
+      }
+    }
+    this.refreshTokenExpirationTimestamp = 0;
+  }
+
+  redirectToLogin(): void {
+    this.router.navigate(['login']);
+  }
+  
+  resetTokens(): void {
+    window.localStorage.removeItem("userId");
+    window.localStorage.removeItem("accessToken");
+    window.localStorage.removeItem("refreshToken");
+    this.accessTokenExpirationTimestamp = 0;
+    this.refreshTokenExpirationTimestamp = 0;
   }
 }
