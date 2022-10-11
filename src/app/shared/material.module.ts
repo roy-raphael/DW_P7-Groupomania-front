b8092919099cb7fe28d2053431fb48cd5eaa305e
Raphael Roy <roy.raphael.33@gmail.com>
2022-10-11 18:39:42 +0200

    feat: add a menu button (no action inside the menu for now) to edit/delete a post (if authorized)

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   exports: [
     MatToolbarModule,
-    MatCardModule
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
   ]
 })
 export class MaterialModule {}
