9571d7a654043903a78a0e71ef18155c243ca9af
Raphael Roy <roy.raphael.33@gmail.com>
2022-10-12 11:03:48 +0200

    feat: display user pseudonym as post title, if it exists

--- src/app/shared/pipes/username.pipe.ts
+++ src/app/shared/pipes/username.pipe.ts
@@ -4,7 +4,8 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'username'
 })
 export class UsernamePipe implements PipeTransform {
-  transform(value: { firstName: string, lastName: string }, locale: 'en' | 'fr' = 'fr'): string {
+  transform(value: { firstName: string, lastName: string, pseudo: string | undefined }, locale: 'en' | 'fr' = 'fr'): string {
+    if (value.pseudo != null) return value.pseudo;
     return locale === 'fr' ?
       `${value.lastName.toUpperCase()} ${value.firstName}` :
       `${value.firstName} ${value.lastName}`;
