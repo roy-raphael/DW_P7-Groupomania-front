e84aa013194e7489024233b908e128192957d13c
Raphael Roy <roy.raphael.33@gmail.com>
2022-10-12 10:26:01 +0200

    feat: update canEditAndDelete property (of post component) with authorId value in new post, user ID, and user role

--- src/app/social-network/components/post/post.component.ts
+++ src/app/social-network/components/post/post.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
 import { Post } from 'src/app/core/models/post.model';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-post',
@@ -10,15 +11,15 @@ export class PostComponent implements OnInit {
   @Input() post!: Post;
   canEditAndDelete!: boolean;
 
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {}
   
   ngOnChanges(changes: SimpleChanges) {
     const newPost: Post = changes['post'].currentValue;
     if (newPost != null) {
-      console.log(newPost);
-      this.canEditAndDelete = true;
+      // console.log(newPost);
+      this.canEditAndDelete = this.authService.isUserAdmin() || this.authService.isUserAuthor(newPost.authorId);
     }
   }
 }
