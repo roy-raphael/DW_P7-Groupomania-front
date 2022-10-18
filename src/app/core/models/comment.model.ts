f0a7df5bc4aa659a70d2ce22628710374a2ca66e
Raphael Roy <roy.raphael.33@gmail.com>
2022-10-18 11:23:06 +0200

    feat: display author of comments

--- src/app/core/models/comment.model.ts
+++ src/app/core/models/comment.model.ts
@@ -1,7 +1,10 @@
+import { User } from "./user.model";
+
 export class Comment {
     id!: string;
     createdAt!: Date;
     text!: string;
     authorId!: string;
+    author!: User;
     postId!: string;
 }
