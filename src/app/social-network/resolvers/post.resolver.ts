d4e16b57173f0d6cf5f9617d0153b58bff5a0061
Raphael Roy <roy.raphael.33@gmail.com>
2022-10-24 16:14:55 +0200

    feat: display error message when resolvers fail

--- src/app/social-network/resolvers/post.resolver.ts
+++ src/app/social-network/resolvers/post.resolver.ts
@@ -2,18 +2,27 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
 import { catchError, EMPTY, Observable } from 'rxjs';
 import { Post } from 'src/app/core/models/post.model';
+import { MessageHandlingService } from 'src/app/core/services/message-handling.service';
 import { PostsService } from 'src/app/core/services/posts.service';
 
 @Injectable()
 export class PostResolver implements Resolve<Post> {
-  constructor(private postsService: PostsService) {}
+  constructor(private postsService: PostsService,
+              private messagehandlingService: MessageHandlingService) {}
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Post> {
     const postId = route.paramMap.get('id');
     if (postId == null) {
+      console.error("Error during PostResolver:resolve : the ID of the post (in the URL params) seems to be null.");
+      this.messagehandlingService.displayError("Erreur pendant la récupération de la publication : elle semble avoir un ID nul. Veuillez essayer une autre publication.");
       return EMPTY;
     } else {
-      return this.postsService.getOnePost(<string>postId).pipe(catchError(() => EMPTY));
+      return this.postsService.getOnePost(<string>postId).pipe(
+        catchError(() => {
+          this.messagehandlingService.displayError("Erreur pendant la récupération de la publication. Veuillez réessayer plus tard.");
+          return EMPTY;
+        })
+      );
     }
   }
 }
--- src/app/social-network/resolvers/posts.resolver.ts
+++ src/app/social-network/resolvers/posts.resolver.ts
@@ -2,13 +2,20 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
 import { catchError, EMPTY, Observable } from 'rxjs';
 import { Post } from 'src/app/core/models/post.model';
+import { MessageHandlingService } from 'src/app/core/services/message-handling.service';
 import { PostsService } from 'src/app/core/services/posts.service';
 
 @Injectable()
 export class PostsResolver implements Resolve<Post[]> {
-  constructor(private postsService: PostsService) {}
+  constructor(private postsService: PostsService,
+              private messagehandlingService: MessageHandlingService) {}
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Post[]> {
-    return this.postsService.getPosts().pipe(catchError(() => EMPTY));
+    return this.postsService.getPosts().pipe(
+      catchError(() => {
+        this.messagehandlingService.displayError("Erreur pendant la récupération des publications. Veuillez réessayer plus tard.");
+        return EMPTY;
+      })
+    );
   }
 }
