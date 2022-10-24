79bcd5f6df45b2457a0efe27fde642dc74d23038
Raphael Roy <roy.raphael.33@gmail.com>
2022-10-24 15:15:36 +0200

    feat: if the login route is blocked for a period, display when the user should retry the login

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -7,6 +7,14 @@ import { AuthService } from 'src/app/core/services/auth.service';
 import { MessageHandlingService } from 'src/app/core/services/message-handling.service';
 
 const EMAIL_REGEXP = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+const TIME_DIFFS = {
+  minute: 60,
+  hour: 60 * 60,
+  day: 24 * 60 * 60,
+  week: 7 * 24 * 60 * 60,
+  month: 30 * 24 * 60 * 60,
+  year: 365 * 24 * 60 * 60
+};
 
 @Component({
   selector: 'app-login',
@@ -43,29 +51,62 @@ export class LoginComponent implements OnInit {
       tap((response: {user: User, accessToken: string, refreshToken: string}) => {
         this.loading = false;
         if (response) {
-          this.resetForm();
+          this.mainForm.reset();
         }
       }),
       catchError(error => {
         this.loading = false;
-        if (error instanceof HttpErrorResponse) {
-          if (error.status === 401) {
-            this.messagehandlingService.displayError("Erreur : utilisateur inconnu ou mot de passe invalide.");
-          } else if (error.status === 429) {
-            this.messagehandlingService.displayError("Erreur : mot de passe invalide. Veuillez attendre avant de réessayer."); // TODO retrieve retry-after
-          } else {
-            this.messagehandlingService.displayError("Erreur du serveur. Veuillez réessayer plus tard.");
-          }
-        } else {
-          this.messagehandlingService.displayError("Erreur pendant la connexion. Veuillez réessayer plus tard.");
-        }
+        this.displayLoginError(error);
         throw error;
       })
     ).subscribe();
   }
 
-  private resetForm() {
-    this.mainForm.reset();
+  private displayLoginError(error: any) {
+    if (error instanceof HttpErrorResponse) {
+      if (error.status === 401 || error.status === 429) {
+        var retryAfterSec = error.error ? error.error.retryAfter : 0;
+        if (retryAfterSec) {
+          var retryAfterString = `${retryAfterSec} secondes`;
+          if (retryAfterSec === 1) {
+            retryAfterString = `1 seconde`;
+          } else if (retryAfterSec < TIME_DIFFS.minute) {
+            retryAfterString = `${retryAfterSec} secondes`;
+          } else if (retryAfterSec < 2 * TIME_DIFFS.minute) {
+            retryAfterString = `1 minute`;
+          } else if (retryAfterSec < TIME_DIFFS.hour) {
+            retryAfterString = `${Math.floor(retryAfterSec / TIME_DIFFS.minute)} minutes`;
+          } else if (retryAfterSec < 2 * TIME_DIFFS.hour) {
+            retryAfterString = `1 heure`;
+          } else if (retryAfterSec < 2 * TIME_DIFFS.day) {
+            retryAfterString = `${Math.floor(retryAfterSec / TIME_DIFFS.hour)} heures`;
+          } else if (retryAfterSec < 2 * TIME_DIFFS.week) {
+            retryAfterString = `${Math.floor(retryAfterSec / TIME_DIFFS.day)} jours`;
+          } else if (retryAfterSec < 2 * TIME_DIFFS.month) {
+            retryAfterString = `${Math.floor(retryAfterSec / TIME_DIFFS.week)} semaines`;
+          } else if (retryAfterSec < 2 * TIME_DIFFS.year) {
+            retryAfterString = `${Math.floor(retryAfterSec / TIME_DIFFS.month)} mois`;
+          } else {
+            retryAfterString = `${Math.floor(retryAfterSec / TIME_DIFFS.year)} ans`;
+          }
+          if (error.status === 401) {
+            this.messagehandlingService.displayError("Erreur : mot de passe invalide. Veuillez réessayer dans " + retryAfterString);
+          } else {
+            this.messagehandlingService.displayError("Erreur : trop de tentatives de connexion échouées (mot de passe invalide). Veuillez réessayer dans " + retryAfterString);
+          }
+        } else {
+          if (error.status === 401) {
+            this.messagehandlingService.displayError("Erreur : utilisateur inconnu ou mot de passe invalide.");
+          } else {
+            this.messagehandlingService.displayError("Erreur : trop de tentatives de connexion échouées (mot de passe invalide). Veuillez attendre avant de réessayer.");
+          }
+        }
+      } else {
+        this.messagehandlingService.displayError("Erreur du serveur. Veuillez réessayer plus tard.");
+      }
+    } else {
+      this.messagehandlingService.displayError("Erreur pendant la connexion. Veuillez réessayer plus tard.");
+    }
   }
   
   getFormControlErrorText(ctrl: AbstractControl) {
