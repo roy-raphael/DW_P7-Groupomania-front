787db06699102efe2a57993895d75474ceae4891
Raphael Roy <roy.raphael.33@gmail.com>
2022-10-25 19:12:27 +0200

    feat: update comments style again

--- src/app/shared/animations/slide-and-fade.animation.ts
+++ src/app/shared/animations/slide-and-fade.animation.ts
@@ -2,13 +2,13 @@ import { animate, animation, style } from '@angular/animations';
 
 export const slideAndFadeAnimation = animation([
     style({
-        transform: 'translateX(-100%)',
+        transform: 'translateX(-5%)',
         opacity: 0,
         'background-color': '#ffd7d7',
     }),
     animate('250ms ease-out', style({
         transform: 'translateX(0)',
         opacity: 1,
-        'background-color': 'white',
+        'background-color': 'transparent',
     })),
 ]);
