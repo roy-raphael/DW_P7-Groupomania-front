69f6c21af27f1ddec5b78a3de62cf8b703b282fa
Raphael Roy <roy.raphael.33@gmail.com>
2022-10-26 14:06:44 +0200

    fix: set firstname first, even for french language

--- src/app/shared/pipes/username.pipe.ts
+++ src/app/shared/pipes/username.pipe.ts
@@ -7,7 +7,7 @@ export class UsernamePipe implements PipeTransform {
   transform(value: { firstName: string, lastName: string, pseudo: string | undefined }, locale: 'en' | 'fr' = 'fr'): string {
     if (value.pseudo != null) return value.pseudo;
     return locale === 'fr' ?
-      `${value.lastName.toUpperCase()} ${value.firstName}` :
+      `${value.firstName} ${value.lastName.toUpperCase()}` :
       `${value.firstName} ${value.lastName}`;
   }
 }
